fb43982e60af82eb9b60c0670ed94e8fff5d8e0d
Lea Ellingwood <leaellingwood@hotmail.com>
2018-11-20 14:21:15 -0500

    finished, i think

--- src/App.js
+++ src/App.js
@@ -1,26 +1,135 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import todoList from "./todos.json";
+import { Route, Switch, Link } from "react-router-dom";
+import TodoList from "./TodoList";
 class App extends Component {
+  state = { todos: todoList };
+
+  handleToggleTodo = todoIdThatWasClicked => event => {
+    const newTodos = this.state.todos.map(todo => {
+      if (todo.id === todoIdThatWasClicked) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    this.setState({
+      todos: newTodos
+    });
+  };
+
+  handleDeleteCompletedTodos = event => {
+    const newTodos = this.state.todos.filter(todo => {
+      if (todo.completed === true) {
+        return false;
+      }
+      return true;
+    });
+    this.setState({
+      todos: newTodos
+    });
+  };
+
+  handleDeleteTodo = todoIdThatWasClicked => event => {
+    const newTodos = this.state.todos.filter(todo => {
+      if (todo.id === todoIdThatWasClicked) {
+        return false;
+      }
+      return true;
+    });
+    this.setState({
+      todos: newTodos
+    });
+  };
+
+  addNewTodo = event => {
+    if (event.keyCode === 13) {
+      let newTodos = this.state.todos.slice(0);
+      let newTodo = {
+        userId: 1,
+        id: Math.floor(Math.random() * 30938409834) + 1,
+        title: event.target.value,
+        completed: false
+      };
+      newTodos.push(newTodo);
+      this.setState({
+        todos: newTodos
+      });
+      console.log(this.state);
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+            onKeyDown={this.addNewTodo}
+          />
         </header>
-      </div>
+        <section className="main">
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <TodoList
+                  todos={this.state.todos}
+                  handleToggleTodo={this.handleToggleTodo}
+                  handleDeleteTodo={this.handleDeleteTodo}
+                />
+              )}
+            />
+            <Route
+              path={"/active"}
+              render={props => (
+                <TodoList
+                  todos={this.state.todos.filter(todo => !todo.completed)}
+                  handleToggleTodo={this.handleToggleTodo}
+                  handleDeleteTodo={this.handleDeleteTodo}
+                />
+              )}
+            />
+            <Route
+              path={"/completed"}
+              render={props => (
+                <TodoList
+                  todos={this.state.todos.filter(todo => todo.completed)}
+                  handleToggleTodo={this.handleToggleTodo}
+                  handleDeleteTodo={this.handleDeleteTodo}
+                />
+              )}
+            />
+          </Switch>
+        </section>
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>0</strong> item(s) left
+          </span>
+          <ul className="filters">
+            <li>
+              <Link to="/">All</Link>
+            </li>
+            <li>
+              <Link to="/active">Active</Link>
+            </li>
+            <li>
+              <Link to="/completed">Completed</Link>
+            </li>
+          </ul>
+          <button
+            className="clear-completed"
+            onClick={this.handleDeleteCompletedTodos}
+          >
+            Clear completed
+          </button>
+        </footer>
+      </section>
     );
   }
 }
---
+++ src/TodoList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+
+class TodoList extends Component {
+    render() {
+      return <ul className="todo-list">
+        {this.props.todos.map(todo => (
+          <TodoItem
+            title={todo.title}
+            key={todo.id}
+            completed={todo.completed}
+            handleToggleTodo={this.props.handleToggleTodo(todo.id)}
+            handleDeleteTodo={this.props.handleDeleteTodo(todo.id)}
+          />
+        ))
+
+
+      }</ul>;
+   
+    }
+  }
+
+
+
+export default TodoList;
+
+
+// this was in App.  Move this here.  Not sure why??
+// TodoItem
+//   key={todo.id}
+//   title={todo.title}
+//   completed={todo.completed}
+//   (name for completing todo)={props.handleCompleteTodoClick(todo.id)}
+//   (name for remove todo)={props.handleRemoveTodoClick(todo.it)}
+
+
+// React.fragment would allow you to add things less than a div to get around the requirement that you only have one div.
+
+// the two ways to re-render the page is to: 1) change the state, and 2) 
